da5419347da198f5f0bb8c316b9d18dc032c5bf6
fglez <80017003+fglez@users.noreply.github.com>
2021-03-30 19:17:46 -0600

    Update phylobject.R
    Remove lines to give phyloseq format
    Remove lines to merge phyloseq objects
    Add new libraries
    Rewrite commetns for Alpha diversity
    Add Alpha diversity plots for Chao1 indexes and interpretation variables
    Optimize Kruskal Wallis test for alpha diversity indexes of every variable in sample_data with the function kt_indata
    Add Wilcoxon test for variables with p<0.1 in Kruskal Wallis test
    Rewrite comments for Beta Diversity
    Optimize PERMANOVA for every variable in sample_data with the function pmav_all
    Add analysis of composition tables and plots

--- fernanda/phylobject.R
+++ fernanda/phylobject.R
@@ -1,143 +1,223 @@
-##Metagenomics R script
-#by Nelly Selem, Diego Garfias and 
-#The code/lesson was retrieved from 
-#https://carpentries-incubator.github.io/metagenomics/09-diversity-analysis/index.html
+###METAGENOMIC ANALYSIS 
 
-#Libraries
+#Some parts of this analysis were modified from the code/lesson retrieved from https://carpentries-incubator.github.io/metagenomics/09-diversity-analysis/index.html
+
+##LIBRARIES
 library("ggplot2")
 library("readr")
 library("phyloseq")
 library("patchwork")
 library("vegan")
+library("dplyr")
+library("ANCOMBC")
+library("tidyverse")
+library("microbiome")
+
+##INITIAL EXPLORATION OF THE MERGED FILE
+
+#Read merged file
+#The merged file can be created with the transform_merge.R script found here https://github.com/nselem/mg-covid/blob/master/fernanda/transform_merge.R
+setwd("/home/mfcg/Descargas/covid/microbiome/taxonomia/phyloseq") #Set path to the merged file
+covid_ps <- read_rds("covid_ups.rds") #Read the merged rds file
 
 
-#Avoid replacing mannually
-#The code wrote in the next paragraph only is useful in the case of how my data is named
-samnum <- "32"
-path <- paste(c("/home/mfcg/Descargas/covid/microbiome/AC1ME2SS", samnum, "_S", samnum, "/KRAKEN/results"), collapse = "")
-rankedwc <- paste(c("AC1ME2SS", samnum, "_kraken.ranked-wc"), collapse = "")
-ltablewc <- paste(c("AC1ME2SS", samnum, "_kraken.lineage_table-wc"), collapse = "")
-psfile <- paste(c("AC1ME2SS", samnum, ".rds"), collapse = "")  
+#PHYLA ABUNDANCE PLOTS
 
-#Path
-setwd(path)
-
-#Reading data
-OTUS <- read_delim(rankedwc,"\t", escape_double = FALSE, trim_ws = TRUE)
-TAXAS <- read_delim(ltablewc, "\t", escape_double = FALSE,
-                    col_types = cols(subspecies = col_character(),
-                                     subspecies_2 = col_character()), trim_ws = TRUE)
-
-abundance <- as.matrix(OTUS[ , -1]) #To avoid that the OTU column is taken as a sample the first column is omitted
-lineages <- as.matrix(TAXAS)
-
-row.names(abundance) <- OTUS$OTU #Their identity is not lost cause their names are saved as rownames before
-row.names(lineages) <- TAXAS$OTU
-
-OTU <- otu_table(abundance, taxa_are_rows = TRUE)
-TAX <- tax_table(lineages)
-
-psmetagenome <- phyloseq(OTU, TAX)
-#psmetagenome <- prune_taxa(taxa_sums(psmetagenome)>10, psmetagenome) #filters OTUs with less than 10 reads, avoid if you want untrimmed data
-write_rds(psmetagenome, file = psfile)
-
-#Merging
-
-setwd("/home/mfcg/Descargas/covid/microbiome/taxonomia/phyloseq") #set path
-
-#The following steps are to avoid automatic replacing of names
-#This steps generate a vector with the names for the samples 1 to 9
-acme0 <- rep("AC1ME2SS0",9) #adding 0 in the prefix of the name cause this is how it is saved
-unums <- as.character(c(1:9)) #numbers as characters
-punto <- ".rds" #the extension
-unombres <- paste(acme0, unums, rep(punto, 9), sep = "") #vector saving the names for samples 1 to 9
-initial_mgps <- read_rds(unombres[1]) #A initial phyloseq object to start merging
-#This steps generate a vector with the names of the following samples 10 to 33, but are useful for bigger numbers, at least up to 99
-acme <- rep("AC1ME2SS", 23) #prefix, can be replaced for any prefix, just repeat it up to the number of samples you have
-nums <- as.character(c(10:32)) #the numbers as characters, if you have a prefix and it is followed by numeration, you can use this
-nombres <- paste(acme, nums, rep(punto, 23), sep = "") #vector saving the file names, it pastes the aforementioned vectors in order, repeat the extension for as many samples you have
-#These steps merge all the phyloseq objects with unit names
-initial_mgps <- read_rds(unombres[1]) #A initial phyloseq object to start merging
-merged_mg_units <- initial_mgps #we need a initial phyloseq object to start merging
-for(i in 2:length(unombres)){ #here we start in number 2 cause we already have the phyloseq object of the first element in unombres
-  pssolo <- read_rds(unombres[i])
-  merged_mg_units <- merge_phyloseq(merged_mg_units, pssolo)
-}
-#These steps merge the phyloseq objects with unit names with the rest of the files
-merged_all_mg <- merged_mg_units #we need a starting phyloseq object, to acelerate the process we will use the merged phyloseq object of the units
-for(i in 1:length(nombres)){ #here we start in one cause none of the elements have their phyloseq number yet
-  pssolo <- read_rds(nombres[i])
-  merged_all_mg <- merge_phyloseq(merged_all_mg, pssolo)
-}
-merged_all_mg <- subset_taxa(merged_all_mg, superkingdom == "Bacteria") #Only Bacteria OTUs are left, this is done in the merged file
-write_rds(merged_all_mg, file = "AC1MESS_untrim.rds")#saving the phyloseq object with all the samples as RDS
-
-#Reading our merged file
-merged_all_mg <- read_rds("AC1MESS_untrim.rds")
-abs_count_plot <- plot_bar(merged_all_mg, fill = "phylum") +
+#Plot the phyla present in your samples
+#Absolute counts
+abs_count_plot <- plot_bar(covid_ps, fill = "phylum") +
   geom_bar(aes(color=phylum, fill=phylum), stat = "identity", position = "stack") +
   ggtitle("Absolute abundance")
-percentages <- transform_sample_counts(merged_all_mg, function(x) x*100 / sum (x))
+
+#Percent
+percentages <- transform_sample_counts(covid_ps, function(x) x*100 / sum (x))
 perc_plot <- plot_bar(percentages, fill = "phylum") +
   geom_bar(aes(color = phylum, fill = phylum), stat = "identity", position = "stack")
 
-#Retrieving the tables from a phyloseq object and saving them as a csv file
-OTUtable <- as.data.frame(otu_table(merged_all_mg))
-TAXtable <- as.data.frame(tax_table(merged_all_mg))
 
-#Reading the tables 
-SAMtable <- read.csv("metadata-covid3.csv", header = TRUE, row.names = 1)
-OTUtable <- read.csv("covid-otu-table.csv", header = TRUE, row.names = 1)
-TAXtable <- read.csv("covid-taxa-table.csv", header = TRUE, row.names = 1)
 
-#to save any changes use the following commands
-#write.csv(OTUtable, "covid-otu-table.csv")
-#write.csv(TAXtable, "covid-taxa-table.csv")
-#write.csv(SAMtable, "metadata-covid3.csv")
+##ALPHA DIVERSITY PLOTS
 
-SAMtable<- sample_data(SAMtable)
-OTUtable<- otu_table(OTUtable, taxa_are_rows = TRUE)
-TAXtable<- tax_table(as.matrix(TAXtable))
-covid_ps <- phyloseq(SAMtable, OTUtable, TAXtable) #integrating all the tables
-covid_ps <- read_rds("covid_ups.rds")
-#write_rds(covid_ps, file = "covid_ups.rds") #saves phyloseq object
+#The following function plot a alpha diversity plot grouping by a variable, vgroup
+#vgroup must be a character vector of length 1
+#the indexes the function will use are chao1, shannon and simpson
+alpha_plot <- function(vgroup){
+  plot_richness(covid_ps, x = vgroup, 
+                measures = c("Chao1", "Shannon", "Simpson"))
+}
 
-#Alpha diversity plots
-plot_richness(covid_ps, x = "Saliva", measures = "Simpson") +
-  geom_boxplot()
-plot_richness(covid_ps, x = "Family", color = "Symptoms", measures = "Simpson")
-plot_richness(covid_ps, x = "Symptoms", measures = "Simpson") +  
-  geom_boxplot()
+#PLOTS GROUP BY A VARIABLE
 
-#Beta diversity
-#This needs two things: 
-#distances, those can vary due to the method used to calculate, the distances can be calculated directly in the ordination function, but they are required for the PERMANOVA
+#Some variables are interpretations, for more info about them, check this script https://github.com/nselem/mg-covid/blob/master/fernanda/interpretation_sam_data.R
+#and the data https://github.com/nselem/mg-covid/blob/master/fernanda/covid_isam_table.csv
+
+PacAlpha <- alpha_plot("Pacient") #By the patient initial test to COVID
+SymAlpha <- alpha_plot("Symptoms") #By the pressence of symptoms 
+#PCRalpha <- alpha_plot("Saliva") #By the result of the PCR result to COVID at the moment of the microbiome. Replaced by "Stage"
+StaAlpha <- alpha_plot("Stage") #By the patient stage of the COVID disease (not infected, infected, post infected). Replaced PCR
+FamAlpha <- alpha_plot("Family") #By each family
+ConAlpha <- alpha_plot("Condition") #By the condition of the COVID disease (control, symtomatyc, asymptomatyc)
+SexAlpha <- alpha_plot("Sex") #By the patient sex
+
+##STATISTICAL TESTS ON ALPHA INDEXES 
+
+#Richness data
+richness <- plot_richness(covid_ps, measures = c("Chao1", "Shannon", "Simpson"))
+richness <- richness$data
+
+#Subset richness data by alpha diversity index
+H <- richness %>% filter(variable == "Shannon") #Just Shannon values
+S <- richness %>% filter(variable == "Simpson") #Just Simpson values
+C <- richness %>% filter(variable == "Chao1") #Just Shannon values
+
+#KRUSKAL-WALLIS
+
+#Kruskal Wallis test for each index subset by a chosen variable
+#Create a vector that contain the variables to group the elements for the Kruskal-Wallis test
+samtable <- sample_data(covid_ps)
+samcols <- colnames(samtable) #This vector contains all the variables from the metadata, even the ones of interpretation
+
+#A function that will display a list with the results of each variable to the Kruskal-Wallis test
+#for a given set of index values, indata
+#This will help explore if there is any significative difference in a variable
+kt_indata <- function(indata) {
+  klist <- vector("list", length(samcols)) #list to save the results of the for cycle
+  names(klist) <- samcols #to preserve the identity of each variable tested in the list
+  for(i in 1:length(samcols)){
+    groupi <- indata[,i]
+    kti <- kruskal.test(indata$value, groupi)
+    klist[[i]] <- as.vector(kti)
+  } 
+  return(klist)
+}
+
+#Kruskal-Wallis results for each variable in the sample data of the phyloseq object, covid_ps
+H_kw_res <- kt_indata(indata = H) #For the Shannon indexes
+S_kw_res <- kt_indata(indata = S) #For the Simpson indexes
+C_kw_res <- kt_indata(indata = C) #For the Chao1 indexes
+
+#WILCOXON
+
+#Wilcoxon test between some groups of interest
+#Only "Saliva" variable, also called "Stage", was chosen. Being the only one with a difference <0.1 in the Kruskal test
+Ssplit <- split(S, S$Saliva) #Split the groups by the PCR result 
+NvC_simsal <- wilcox.test(Ssplit$Negative$value, Ssplit$Negative_Control$value) #Negative(or post-covid) vs control
+PvC_simsal <- wilcox.test(Ssplit$Positive$value, Ssplit$Negative_Control$value) #Positive(or infected) vs control
+PvN_simsal <- wilcox.test(Ssplit$Positive$value, Ssplit$Negative$value) #Positive(infected) vs negative(post-covid)
+
+
+
+##BETA DIVERSITY
+
+#To obtain the beta diversity two things are needed: distances and ordination method.
+
+#DISTANCES 
+#These can vary due to the method used to calculate them.
+#Distances can be calculated directly in the ordination function, but they are required as individual object for the PERMANOVA
+#Two methods were chosen for the distances:
 jc_dist <- phyloseq::distance(covid_ps, method = "jaccard") #Jaccard is a presence/absence based distance
 bc_dist <- phyloseq::distance(covid_ps, method = "bray") #Bray-Curtis is an abundance based distance
-#jc_coa_ord <- phyloseq::distance(covid_ps, method = "unifrac") #Unifrac unweighted is a phylogeny based distance, it requires a tree in the phyloseq object
-#and an ordination method, the method here is PCoA, Principal Coordinates Analysis
-jc_coa_ord <- phyloseq::ordinate(covid_ps, method = "PCoA", distance = "jaccard") 
-bc_coa_ord <- phyloseq::ordinate(covid_ps, method = "PCoA", distance = "bray")
-#uf_coa_ord <- phyloseq::ordinate(covid_ps, method = "DPCoA", distance = "unifrac")  #DPCoA can also be used but requires a tree in the phyloseq file
-#PCoA plots grouped by saliva PCR results
-jc_saliva <- plot_ordination(covid_ps, jc_coa_ord, color = "Saliva") + stat_ellipse()
-bc_saliva <- plot_ordination(covid_ps, bc_coa_ord, color = "Saliva") + stat_ellipse()
-#PCoA plots grouped by the symptoms presence absence
-jc_symptoms <- plot_ordination(covid_ps, jc_coa_ord, color = "Symptoms") + stat_ellipse()
-bc_symptoms <- plot_ordination(covid_ps, bc_coa_ord, color = "Symptoms") + stat_ellipse()
-#PCoa plots grouped by family
-jc_fam <- plot_ordination(covid_ps, jc_coa_ord, color = "Family") + stat_ellipse()
-bc_fam <- plot_ordination(covid_ps, bc_coa_ord, color = "Family") + stat_ellipse()
-#Combining info in PCoA
-jc_fs <- plot_ordination(covid_ps, jc_coa_ord, color = "Family", shape = "Symptoms")
-bc_fs <- plot_ordination(covid_ps, bc_coa_ord, color = "Family", shape = "Symptoms")
+#Other useful distance method is Unifrac unweighted/weighted, a phylogeny based one, it requires a tree in the phyloseq object
+
+#ORDINATION METHOD
+#The chosen method for this work is Principal Coordinates Analysis or PCoA
+jc_coa_ord <- phyloseq::ordinate(covid_ps, method = "PCoA", distance = "jaccard") #Jaccard distances
+bc_coa_ord <- phyloseq::ordinate(covid_ps, method = "PCoA", distance = "bray") #Bray distances
+#Double Principal Coordinate Analysis or DPCoA can also be used but requires a tree in the phyloseq file
+
+#PCoA PLOTS BY 
+
+#Saliva PCR results
+jc_saliva <- plot_ordination(covid_ps, jc_coa_ord, color = "Saliva") + stat_ellipse() #Jaccard distances
+bc_saliva <- plot_ordination(covid_ps, bc_coa_ord, color = "Saliva") + stat_ellipse() #Bray-Curtis distances
+
+#Family
+jc_fam <- plot_ordination(covid_ps, jc_coa_ord, color = "Family") + stat_ellipse() #Jaccard distances
+bc_fam <- plot_ordination(covid_ps, bc_coa_ord, color = "Family") + stat_ellipse() #Bray-Curtis distances
+
+#Condition
+jc_con <- plot_ordination(covid_ps, jc_coa_ord, color = "Condition") + stat_ellipse() #Jaccard distances
+bc_con <- plot_ordination(covid_ps, bc_coa_ord, color = "Condition") + stat_ellipse() #Bray-Curtis distances
+
+#Pacient
+jc_pac <- plot_ordination(covid_ps, jc_coa_ord, color = "Pacient") + stat_ellipse() #Jaccard distances
+bc_pac <- plot_ordination(covid_ps, bc_coa_ord, color = "Pacient") + stat_ellipse() #Bray-Curtis distances
+
+#Sex
+jc_sex <- plot_ordination(covid_ps, jc_coa_ord, color = "Sex") + stat_ellipse() #Jaccard distances
+bc_sex <- plot_ordination(covid_ps, bc_coa_ord, color = "Sex") + stat_ellipse() #Bray-Curtis distances
 
 
-#PERMANOVA, Permutational multivariate analysis of variance 
-adonis(jc_dist ~ sample_data(covid_ps)$Saliva) #Permanova using jaccard distances for Saliva results
-adonis(bc_dist ~ sample_data(covid_ps)$Saliva) #Permanova using Bray-Curtis distances for Saliva results
-adonis(jc_dist ~ sample_data(covid_ps)$Symptoms) #Permanova using jaccard distances for the symptoms presence
-adonis(bc_dist ~ sample_data(covid_ps)$Symptoms) #Permanova using Bray-Curtis distances for the symptoms presence
-adonis(jc_dist ~ sample_data(covid_ps)$Family) #Analysis on Family using Jaccard distances
-adonis(bc_dist ~ sample_data(covid_ps)$Family) #Analysis on Family using Bray-Curtis distances
+
+#PERMUTATIONAL MULTIVARIATE ANALYSIS OF VARIANCE
+
+#The following script list the PERMANOVA results for each variable of a given sample_data table
+# for a phyloseq distances object created by the ordination method of choice
+pmav_all <- function(distances){ #distances is distance object created by phyloseq::distance function
+  pmavlist <- vector("list", length(samcols)) #empty list to save the PERMANOVA results
+  names(pmavlist) <- samcols #sample_data variables
+  for(i in 1:length(samcols)){
+    rhsi <- c(as.vector(unlist(samtable[,i]))) #create independent variable vector from the columns of the sample_data table
+    set.seed(100) #establish a seed so the p-values are the same in each repetition
+    pmavi <- adonis(distances ~ rhsi, permutations = 1000)
+    pmavlist[[i]] <- pmavi
+  }
+  return(pmavlist)
+}
+
+#PERMANOVA RESULTS
+#List with the PERMANOVA results using every variable of the sample data of the phyloseq object, covid_ps
+#These variables are used as independent variables, RHS, in the PERMANOVA, to test a dissimilarity matrix, LHS
+#See adonis function, ?adonis
+jc_pmav <- pmav_all(jc_dist) #PERMANOVA results using Jaccard distances
+bc_pmav <- pmav_all(bc_dist) #PERMANOVA results using Bray-Curtis distances
+
+
+
+#DIFFERENTIAL ABUNDANCE
+
+#ANALYSIS OF COMPOSITION OR ANCOM
+#code modified from https://github.com/FrederickHuangLin/ANCOM-BC-Code-Archive/blob/master/scripts/figure_6_table_s1_s2_s3.Rmd
+# and https://rdrr.io/github/FrederickHuangLin/ANCOMBC/f/vignettes/ANCOMBC.Rmd
+
+#Group to a taxa rank
+rankovid <- aggregate_taxa(covid_ps, "phylum")
+
+#Analisys of composition, choosing your variable and values
+ancovid <- ancombc(phyloseq = rankovid, formula = "Stage", 
+                   p_adj_method = "holm", zero_cut = 0.90, lib_cut = 1000,
+                   group = "Stage", struc_zero = TRUE, neg_lb = TRUE, tol = 1e-5,
+                   max_iter = 100, conserve = TRUE, alpha = 0.05, global = TRUE)
+#Make tables with the data to plot
+#Part 1 retrieve and adjust the log fold change between the pairwise comparisons
+dfig_pt1 <- data.frame(ancovid$res$beta * ancovid$res$diff_abn, check.names = FALSE) %>%
+  rownames_to_column("taxon_id")
+#Part 2 retrieve and adjust the standard error between the pairwise comparisons
+dfig_pt2 <- data.frame(ancovid$res$se * ancovid$res$diff_abn, check.names = FALSE) %>%
+  rownames_to_column("taxon_id")
+colnames(dfig_pt2)[-1] <- paste0(colnames(dfig_pt2)[-1], "SD") #Names to distinguish each value
+#Paste together part 1 and 2, filter the comparison you wish to plot
+dfig_c <- dfig_pt1 %>%
+  left_join(dfig_pt2, by = "taxon_id") %>%
+  filter(StageInfection != 0) %>%
+  arrange(desc(StageInfection))
+dfig_c$taxon_id <- factor(dfig_c$taxon_id, levels = dfig_c$taxon_id)
+
+#Log fold change plot
+ggplot(data = dfig_c, #Data is adjusted to plot only the top twenty changes
+           aes(x = taxon_id, y = StageInfection)) +
+  geom_bar(stat = "identity", width = 0.75, position = position_dodge(width = 0.25)) +
+  geom_errorbar(aes(ymin = StageInfection - StageInfectionSD, 
+                    ymax = StageInfection + StageInfectionSD), 
+                width = 0.2, position = position_dodge(0.05), color = "black") +
+  geom_abline(slope = 0, intercept = 0, linetype="dashed", alpha=0.5)+
+  labs(x=NULL, y="Log Fold Change") + 
+  coord_flip()
+
+
+samp_frac <- ancovid$samp_frac
+samp_frac[is.na(samp_frac)] <- 0
+
+log_obs_abn <- log(abundances(covid_ps) + 1)
+log_oabn_adj <- t(t(log_obs_abn)-samp_frac)
 
