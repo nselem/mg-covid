914a77c65688b6917f980e3b4f52ed3ba300be0a
fglez <80017003+fglez@users.noreply.github.com>
2021-06-22 01:03:56 -0500

    Add shell script to execute in mazorka
    This shell script will produce three files to be used as request in mazorka.
    
    It should be executed as 
    sh mzrk-bin-stps.sh file-with-forward-reads.fastq file-with-reverse-reads-fastq prefix-to-identify-your sample
    in a directory that contains IDBA contig file, contif.fa, and both read files if fq format
    i.e.
    cd /PATH/WITH/CONTIG/AND/READS
    sh mzrk-bins-stps reads1.fq reads2.fq sample00
    
    First step 1bowsam
    Uses bowtie and samtools
    It produces a sam file with the mapped and sorted reads
    There is more clarification in the 1mag-bowsam.txt file in this folder
    
    Second step 2metabat
    Uses metabat
    It produces the bins
    There is more clarification in the 2mag-metabat.txt file in this folder
    
    Third step 3checkm
    Uses checkM
    It checks the bins and produces a report
    There is more clarification in the 3mag-checkm.txt file in this folder

---
+++ fernanda/mzrk-bin-stps.sh
@@ -0,0 +1,69 @@
+#!/bin/bash
+
+READ1=$1 #Contains the forward reads in fastq.gz format
+READ2=$2 #Contains the reverse reads in fastq.gz format
+prefix=$3 #An assigned name to identify your sample
+path=$(pwd)
+sign='$'
+
+cat > 1bowsam-${prefix}.sh << EOF
+#PBS -N bowtie2
+#PBS -q default
+#PBS -l nodes=1:ppn=8,mem=24g,vmem=24g,walltime=99:00:00
+#PBS -e 1bowsam${prefix}.err
+#PBS -o 1bowsam${prefix}.out
+#PBS -V
+
+cd $path
+
+module load bowtie2/2.3.5.1
+
+module load samtools/1.9
+
+mkdir bowtie/
+
+bowtie2-build contig.fa bowtie/${prefix}.ind
+
+bowtie2 -p 8 -x bowtie/${prefix}.ind -1 $READ1 -2 $READ2 -S bowtie/${prefix}.map.sam
+
+samtools sort -o bowtie/${prefix}.map.sorted.bam -O bam bowtie/${prefix}.map.sam
+
+EOF
+
+cat > 2metabat-${prefix}.sh << EOF
+#PBS -N MetaBat2-bin
+#PBS -q default
+#PBS -l nodes=1:ppn=8,mem=32g,vmem=32g,walltime=99:00:00
+#PBS -e 2metabat${prefix}.err
+#PBS -o 2metabat${prefix}.out
+#PBS -V
+
+cd $path
+
+module load metabat/2.13
+
+mkdir metabat/
+
+jgi_summarize_bam_contig_depths --outputDepth metabat/${prefix}depth.txt bowtie/${prefix}.map.sorted.bam
+
+metabat2 --saveCls -i contig.fa -a metabat/${prefix}depth.txt -o metabat/${prefix}bin -m 2000  --maxEdges 600
+
+EOF
+
+cat > 3checkm-${prefix}.sh << EOF
+#PBS -q default
+#PBS -l nodes=1:ppn=8,walltime=9:59:59,vmem=32gb,mem=32gb
+#PBS -N check${prefix}
+#PBS -V
+#PBS -o 3check${prefix}.out
+#PBS -e 3check${prefix}.err
+
+cd $path
+
+module load CheckM/1.1.3 Prodigal/2.6.2 hmmer/3.1b2
+
+mkdir checkm/
+
+checkm taxonomy_wf domain Bacteria -t 8 -x fa metabat/ checkm/
+
+EOF
