d671abb99e94a19852a24bdbdce8fae1be43d8c6
fglez <80017003+fglez@users.noreply.github.com>
2021-06-27 12:24:03 -0500

    Solve issues in R script
    Change the word removed so it does not change the names of the bins
    Modify steps order so the data frame, valdf, is numerical
    Add some comands to visualize each result to make sure everything is fine, it can be removed by comenting the lines 32, 40 and 42

--- fernanda/5mag-gbins.R
+++ fernanda/5mag-gbins.R
@@ -19,7 +19,7 @@ smfile <- read.csv("smallfile.csv", header = FALSE) #Read the file
 #Format the data frame smfile
 
 #Remove words to leave only values
-torm <- c("bin", "'Completeness': ", "'Contamination': ") #words to remove must be equal in size to the number of columns
+torm <- c("NOTHING", "'Completeness': ", "'Contamination': ") #words to remove must be equal in size to the number of columns
 valdf <- t(apply(smfile, 1, str_remove_all, torm)) #Apply by row the str_remove_all function and using t to re-arrange columns and rows
 
 #Name the columns
@@ -27,15 +27,17 @@ titulos <- c("bin", "completeness", "contamination") #column names
 colnames(valdf) <- titulos
 
 #Make numeric the completeness and contamination columns
-valdf[ ,2:3] <- apply(valdf[ ,2:3], 2, as.numeric) #Apply by column the as.numeric function
-
 valdf <- as.data.frame(valdf)
+valdf[ ,2:3] <- apply(valdf[ ,2:3], 2, as.numeric) #Apply by column the as.numeric function
+valdf
 
 #Identify the quality bins
 comp <- 80 #Mininum completeness, change it as it fits for you
 cont <- 10 #Maximum contamination allowed, choose a treshold
 good <- valdf %>% 
-  filter(completeness > comp) %>% 
-  filter(contamination < cont)
-goodbins <- good$bin
-write(goodbins, file = "goodbins.ls")
+  filter(completeness > comp) %>% #Leave only the bins above the completeness treshold
+  filter(contamination < cont) #Leave only the bins below the contamination treshold
+good
+goodbins <- good$bin #Retrieve the names of the bins with the desired quality
+goodbins
+write(goodbins, file = "goodbins.ls") #Make a txt file with the bins' names
